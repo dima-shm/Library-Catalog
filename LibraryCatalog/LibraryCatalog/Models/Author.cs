53ed943b4dca2cb20df458748d9b889ef2adc6f9
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-17 12:39:25 +0300

    Add models

---
+++ LibraryCatalog/LibraryCatalog/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_3.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Surname { get; set; }
+
+        public string Name { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Surname} {Name} {Patronymic}";
+        }
+    }
+}
---
+++ LibraryCatalog/LibraryCatalog/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab_3.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [Required, ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Year { get; set; }
+
+        public int NumPages { get; set; }
+
+        public int Price { get; set; }
+
+        public virtual Author Author { get; set; }
+    }
+}
