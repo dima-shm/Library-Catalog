1ef757e2ba19c7b16b5d2d9d31371603011d150d
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-17 12:49:15 +0300

    Enable migrations

---
+++ LibraryCatalog/LibraryCatalog/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace LibraryCatalog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Lab_3.Context.ApplicationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Lab_3.Context.ApplicationContext";
+        }
+
+        protected override void Seed(Lab_3.Context.ApplicationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
