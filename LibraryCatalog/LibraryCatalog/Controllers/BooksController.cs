2f8ce347f8af4e9b8213b1116f81c0fb6f3f5c4c
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-17 12:45:18 +0300

    Added Books controller and views

---
+++ LibraryCatalog/LibraryCatalog/Controllers/BooksController.cs
@@ -0,0 +1,115 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Lab_3.Context;
+using Lab_3.Models;
+
+namespace LibraryCatalog.Controllers
+{
+    public class BooksController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        public ActionResult Index()
+        {
+            var books = db.Books.Include(b => b.Author);
+            return View(books.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.AuthorId = new SelectList(db.Authors, "Id", "Surname");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Id,Title,AuthorId,Description,Year,NumPages,Price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorId = new SelectList(db.Authors, "Id", "Surname", book.AuthorId);
+            return View(book);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorId = new SelectList(db.Authors, "Id", "Surname", book.AuthorId);
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,AuthorId,Description,Year,NumPages,Price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorId = new SelectList(db.Authors, "Id", "Surname", book.AuthorId);
+            return View(book);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Books.Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
