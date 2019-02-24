cb6150e374f9ad3afb16d358921eb0c958b1bd5a
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-24 16:57:44 +0300

    Update WebApiConfig.cs and added Entity Framework packages

--- LibraryCatalogWebApi/LibraryCatalogWebApi/App_Start/WebApiConfig.cs
+++ LibraryCatalogWebApi/LibraryCatalogWebApi/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace LibraryCatalogWebApi
 {
@@ -9,11 +6,7 @@ namespace LibraryCatalogWebApi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Конфигурация и службы веб-API
-
-            // Маршруты веб-API
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
