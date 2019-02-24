8ea8ae863cdddd4adfc10e950e20f8f44e980595
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-24 17:02:38 +0300

    Added Models

---
+++ LibraryCatalogWebApi/LibraryCatalogWebApi/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryCatalogWebApi.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [Required, ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
+        public string Description { get; set; }
+
+        public int Year { get; set; }
+
+        public int NumPages { get; set; }
+
+        public virtual Author Author { get; set; }
+    }
+}
