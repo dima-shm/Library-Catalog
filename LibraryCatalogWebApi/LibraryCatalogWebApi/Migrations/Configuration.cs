a354bd0f2c23dba6d78a03f5996155b9d302b1e9
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-24 17:08:02 +0300

    enable migrations

---
+++ LibraryCatalogWebApi/LibraryCatalogWebApi/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace LibraryCatalogWebApi.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LibraryCatalogWebApi.Context.ApplicationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LibraryCatalogWebApi.Context.ApplicationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
