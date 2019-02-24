0e165bcdd6fe4d4316efbdb680bf7b96ad5c2227
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-24 17:08:52 +0300

    Add migration

---
+++ LibraryCatalogWebApi/LibraryCatalogWebApi/Migrations/201902241408198_DataMigration.cs
@@ -0,0 +1,46 @@
+namespace LibraryCatalogWebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Surname = c.String(),
+                        Name = c.String(),
+                        Patronymic = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        AuthorId = c.Int(nullable: false),
+                        Description = c.String(),
+                        Year = c.Int(nullable: false),
+                        NumPages = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.AuthorId, cascadeDelete: true)
+                .Index(t => t.AuthorId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.Books", new[] { "AuthorId" });
+            DropTable("dbo.Books");
+            DropTable("dbo.Authors");
+        }
+    }
+}
