6c00ca4be861a6309555935be4e3c9dfcccde448
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-24 17:03:17 +0300

    Added context

---
+++ LibraryCatalogWebApi/LibraryCatalogWebApi/Context/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using LibraryCatalogWebApi.Models;
+using System.Data.Entity;
+
+namespace LibraryCatalogWebApi.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public ApplicationContext() : base("LibraryCatalogDB") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+}
